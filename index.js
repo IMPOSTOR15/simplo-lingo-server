b4dbdd3a33e63d7bea5f8041fd285ee62664abc3
Andrey Dagaev <dandr212@gmail.com>
2023-08-06 19:16:38 +0300

    try allow headers

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const app = express()
 //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 //     credentials: true
 //   }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
