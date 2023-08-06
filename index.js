50f8d4787af2812ece9037c33cc1b0ff27cedbd1
Andrey Dagaev <dandr212@gmail.com>
2023-08-06 19:30:06 +0300

    another cors

--- index.js
+++ index.js
@@ -9,19 +9,11 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 const app = express()
-// app.use(cors({
-//     origin: 'https://simplo-lingo.netlify.app',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true
-//   }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-  
+app.use(cors({origin: 'https://simplo-lingo.netlify.app'}));
+
+
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
